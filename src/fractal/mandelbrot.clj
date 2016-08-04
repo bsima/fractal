cf25cc3cbfc44f9348c60a6ac001b66cb92fdc16
Ben Sima <bensima@gmail.com>
2016-08-03 17:50:01 -0700

    wip

--- src/fractal/mandelbrot.clj
+++ src/fractal/mandelbrot.clj
@@ -16,22 +16,6 @@
 ;;
 ;; Source: http://nakkaya.com/2009/09/29/fractals-in-clojure-mandelbrot-fractal/
 
-(defn calc-iterations
-  [p q max-iterations]
-  (let [c (Complex. p q)]
-    (loop [z c
-           i 0]
-      (cond
-        (= 0 i)
-        0
-
-        (or (> (.abs z) 2.0)
-            (> i max-iterations))
-        (recur (.add c (.multiply z z)) (inc i))
-
-        :else
-        (- i 1)))))
-
 
 (defn calc-iterations
   [p q max-iterations]
@@ -56,10 +40,12 @@
           g    (min (int (/ (* 5 (* gray gray)) 255)) 255)
           b    (min (int (+ 40 (/ (* 5 (* gray gray)) 255))) 255)]
       (Color. r g b))))
+ 
 
+;; TODO convert to quil
 (defn generate
-  [x y width height max-iterations
-   graphics surface-width surface-height]
+  [x y width height max-iterations graphics
+    surface-width surface-height]
   (doseq [i (range surface-width)
           j (range surface-height)]
     (let [p          (+ x (* width  (/ i surface-width)))
@@ -83,11 +69,40 @@
                    (.drawImage g image 0 0 this)))]
     (generate x y width height iterations
               (.createGraphics image) surface-width surface-height)
-    (print @a)
     (doto (JFrame.)
       (.add canvas)
       (.setSize (Dimension. surface-width surface-height))
       (.show))))
 
+
 (defn doit []
   (draw -2.1 -1.5 3.0 3.1 100 510 510))
+
+
+(comment
+  (doit)
+  )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; QUIL
+
+
+(defn quil-calc-pixel-color
+  [i max-iterations]
+  (if (or (< i 10)
+          (= i max-iterations))
+    (quil/color 0 0 0)
+    (let [gray (int (/ (* i 255) max-iterations))
+          r    gray
+          g    (min (int (/ (* 5 (* gray gray)) 255)) 255)
+          b    (min (int (+ 40 (/ (* 5 (* gray gray)) 255))) 255)]
+      (quil/color r g b))))
+
+
+
+(quil/defsketch
+  :host "host"
+  :size [510 510]
+  :draw
+  (fn []
+    ))
